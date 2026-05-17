c11200cf37c3e6c8dd4b46da4bc49ad0e0064239
Connor <connor@Connors-MBP.Home>
2026-05-17 18:46:00 +0100

    image pathing fix

--- src/data.js
+++ src/data.js
@@ -2,14 +2,14 @@ export const projects = [
   {
     title: 'React Calculator',
     description: 'A calculator app built using React.js with full interactivity and modern styling.',
-    image: '/Images/react-calc.png',
+    image: './Images/react-calc.png',
     live: 'https://con-grieves.github.io/react-calc/',
     code: 'https://github.com/con-grieves/react-calc',
   },
   {
     title: 'Javascript Dice Game',
     description: 'A lightweight 2 player dice game.',
-    image: '/Images/dice.png',
+    image: './Images/dice.png',
     live: 'https://con-grieves.github.io/Dice/',
     code: 'https://github.com/con-grieves/Dice',
   },
